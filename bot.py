0192a525a124a58dac09275cd166db2ad94213d8
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-10-20 11:10:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from pyrogram import Client , filters
+
 from pymongo import MongoClient
 import os
+
+from pyrogram.types import Message
 
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
