474dcaf624f89f673ce4ca7e47b74b35aa6832bc
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-11-13 22:25:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 from pyrogram import Client , filters
-import pymongo
-from pymongo import MongoClient
+from pyrogram.types import Message, ChatPermissions
+import asyncio
 import os
 
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-MONGO_URL = os.environ.get("MONGO_URL")
+#MONGO_URL = os.environ.get("MONGO_URL")
 
-G = -1001686630791
+G = -1001525634215
 
 bot = Client(
     "Level" ,
@@ -18,46 +18,30 @@ bot = Client(
     bot_token = BOT_TOKEN
 )
 
-@bot.on_message(filters.command("test"))
-async def rank(client, message):
-    await message.reply_text(f"🎆Added")
+Pic = "https://telegra.ph/file/59c987c5c838e973e8d3f.jpg"
+@bot.on_message(filters.new_chat_members & filters.chat(G))
+async def welcome(_, m: Message):
+        im = m.from_user.id
+        nm = m.from_user.mention
+        unm = m.from_user.username
+        msg = await bot.send_photo(G, photo=Pic, caption=f"👤{nm} [ @{unm} ] \n⚠️You won't be able to use MEDIA in group due to security purpose! \n**You need to be atleast 2 weeks here AND 100+ message sended**\n__Then you'll be authorised to send Media__")
+        await bot.restrict_chat_member(G, im, ChatPermissions(can_send_messages=True, can_invite_users=True, can_send_polls=True, can_send_other_messages=False, can_send_media_messages=False))
+        await msg.delete()
 
-leveldb = MongoClient(MONGO_URL)
-level = leveldb["LEVEL"]["mem_LVL"]
+@bot.on_message(filters.command("start") & filters.private)
+async def ping(_, message: Message):
+    await message.reply_text("Pretty much nothing for Normies \n Join @Anime_Gaming_Chat_Global")
 
+@bot.on_message(filters.command("settings") & filters.private)
+async def ping(_, message: Message):
+    uid = message.from_user.id
+    mystic = await message.reply_text("Getting User Unique id...")
+    await asyncio.sleep(1)
+    await mystic.edit(f"👥: {uid}")
+    await asyncio.sleep(1)
+    await mystic.edit("UID Not Found In D.B.")
+    await asyncio.sleep(1)
+    await mystic.edit("⚠️Only Authorised Users Can use This command")
 
-@bot.on_message(filters.command("top2") & filters.chat(G))
-async def rank(client, message):
-    tl = level.find().sort("xp")
-    dt1 = [x for x in level.find().sort('xp',pymongo.DESCENDING)][:10]
-    texto = "🏆 TOP 10 PLAYERS 🏆\n\n"
-    num = 0
-    for x in dt1:
-        xp = x["xp"]
-        l = 0
-        while True:
-            if xp < ((125*(l**2))+(125*(l))):
-                break
-            l += 1
-        xp -= ((125*((l-1)**2))+(125*(l-1)))
-        fxp = f"{int(xp * 4)}/{int(2000 *((1/2) * l))}"
-        try:
-            users = await bot.get_users(x['USER_ID'])
-            mention = "[" + users.first_name + "](tg://user?id=" + str(users.id) + ")" or users.first_name
-        except Exception:
-            mention = x['USER_ID']
-        num += 1
-        texto += f"{num}》{mention} (ʟᴠʟ: {l} ⟐ ᴇxᴘ: {fxp})\n"
-    await message.reply_text(texto)
-
-
-
-
-
-@bot.on_message(filters.command("top1"))
-async def rank(client, message):
-    tl = level.find().sort("xp")
-    dt1 = [x for x in level.find().sort('xp',pymongo.DESCENDING)][:10]
-    await message.reply_text(dt1)
 
 bot.run() 
