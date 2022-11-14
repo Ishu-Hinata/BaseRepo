8ee580c32ce20c5872ed79c7f3c6671d36310d98
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-11-14 11:21:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ bot = Client(
 
 Pic = "https://telegra.ph/file/59c987c5c838e973e8d3f.jpg"
 @bot.on_message(filters.new_chat_members & filters.chat(G))
-async def welcome(_, m: Message):
+async def welcome(bot, m: Message):
         im = m.from_user.id
         nm = m.from_user.mention
         unm = m.from_user.username
@@ -28,12 +28,16 @@ async def welcome(_, m: Message):
         await asyncio.sleep(10)
         msg = await bot.send_photo(G, photo=Pic, caption=f"👤{nm} [ @{unm} ] \n⚠️You won't be able to use MEDIA in group due to security purpose! \n**You need to be atleast 2 weeks here AND 100+ message sended**\n__Then you'll be authorised to send Media__")
 
+@bot.on_message(filters.new_chat_members & filters.chat(G))
+async def welcome(bot, m: Message):
+    await message.reply_text("!bot reloaded")
+
 @bot.on_message(filters.command("start") & filters.private)
-async def ping(_, message: Message):
+async def ping(bot, message: Message):
     await message.reply_text("Pretty much nothing for Normies \n Join @Anime_Gaming_Chat_Global")
 
 @bot.on_message(filters.command("settings") & filters.private)
-async def ping(_, message: Message):
+async def ping(bot, message: Message):
     uid = message.from_user.id
     mystic = await message.reply_text("Getting User Unique id...")
     await asyncio.sleep(1)
