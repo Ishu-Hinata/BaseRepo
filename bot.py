8d7d628820150d07e36c2dc1661167a199e78aa6
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-11-16 11:45:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@ from pyrogram import Client , filters, idle
 from pyrogram.types import Message, ChatPermissions
 import asyncio
 import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
@@ -27,6 +31,108 @@ async def new(bot, message):
         msg = await message.reply_photo(photo=Pic, caption=f"👤{nm} [ @{unm} ] \n⚠️You won't be able to use MEDIA in group due to security purpose! \n**You need to be atleast 2 weeks here AND 100+ message sended**\n__Then you'll be authorised to send Media__")
 
 
+REMOVEBG_API = "KMFFqrBYBocDHBYbz8Gwa2np"
+UNSCREEN_API = "zhLmhhqxbYrKbXrsw5bbnKeo"
+
+@bot.on_message(filters.private & (filters.photo | filters.video | filters.document))
+async def remove_background(bot, update):
+    if not (REMOVEBG_API or UNSCREEN_API):
+        await update.reply_text(
+            text="Error :- API not found",
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=ERROR_BUTTONS
+        )
+        return
+    await update.reply_chat_action("typing")
+    message = await update.reply_text(
+        text="Processing",
+        quote=True,
+        disable_web_page_preview=True
+    )
+    try:
+        new_file_name = f"./{str(update.from_user.id)}"
+        if (
+            update.photo or (
+                update.document and "image" in update.document.mime_type
+            )
+        ):
+            new_file_name += ".png"
+            file = await update.download()
+            await message.edit_text(
+                text="Photo downloaded successfully. Now removing background.",
+                disable_web_page_preview=True
+            )
+            new_document = removebg_image(file)
+        elif (
+            update.video or (
+                update.document and "video" in update.document.mime_type
+            )
+        ):
+            new_file_name += ".webm"
+            file = await update.download()
+            await message.edit_text(
+                text="Video downloaded successfully. Now removing background.",
+                disable_web_page_preview=True
+            )
+            new_document = removebg_video(file)
+        else:
+            await message.edit_text(
+                text="Media not supported",
+                disable_web_page_preview=True,
+                reply_markup=ERROR_BUTTONS
+            )
+        try:
+            os.remove(file)
+        except:
+            pass
+    except Exception as error:
+        await message.edit_text(
+            text=error,
+            disable_web_page_preview=True
+        )
+    try:
+        with open(new_file_name, "wb") as file:
+            file.write(new_document.content)
+        await update.reply_chat_action("upload_document")
+    except Exception as error:
+        await message.edit_text(
+           text=error,
+           reply_markup=ERROR_BUTTONS
+        )
+        return
+    try:
+        await update.reply_document(
+            document=new_file_name,
+            quote=True
+        )
+        try:
+            os.remove(new_file_name)
+        except:
+            pass
+    except Exception as error:
+        await message.edit_text(
+            text=f"Error:- `{error}`",
+            disable_web_page_preview=True,
+            reply_markup=ERROR_BUTTONS
+        )
+
+
+def removebg_image(file):
+    return requests.post(
+        "https://api.remove.bg/v1.0/removebg",
+        files={"image_file": open(file, "rb")},
+        data={"size": "auto"},
+        headers={"X-Api-Key": REMOVEBG_API}
+    )
+
+
+def removebg_video(file):
+    return requests.post(
+        "https://api.unscreen.com/v1.0/videos",
+        files={"video_file": open(file, "rb")},
+        headers={"X-Api-Key": UNSCREEN_API}
+    )
 
 
 
@@ -51,7 +157,7 @@ async def settings(bot, message):
     await asyncio.sleep(1)
     await mystic.edit("⚠️Only Authorised Users Can use This command")
 
-##okok 
+##
 
 bot.run() 
 idle()
