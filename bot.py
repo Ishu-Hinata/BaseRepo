a8920605fbe9bb1f3eb1e03570fc043321de4b59
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-11-19 18:56:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,16 +29,19 @@ NK = "Required Admin permission To Send Messages in Group"
 async def ban_all(_,msg):
     chat_id=msg.chat.id    
     bot=await app.get_chat_member(chat_id,BOT_ID)
-    bot_permission=bot.privileges.can_restrict_members==True    
+    bot_permission=bot.privileges.can_restrict_members==True
+    if not bot_permission:
+        await msg.reply_text(NK)
     if bot_permission:
+        await msg.reply_text(SK)
         async for member in app.get_chat_members(chat_id):       
             try:
                     await app.ban_chat_member(chat_id, member.user.id)
-                    await msg.reply_text(f"ғᴜᴄᴋɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴀɴᴅ ᴛʜᴇɪʀ ᴍᴏᴍs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {member.user.mention}")                    
+                    await msg.delete()                   
             except Exception:
                 pass
     else:
-        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
+        await msg.reply_text(NK)
                                                                             
 
 @app.on_message(filters.command("r"))
