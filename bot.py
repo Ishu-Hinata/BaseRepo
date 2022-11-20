a499b09d16ab7ade8e1f88adb5521ce8025042ba
Ishu <87901742+Ishu-Hinata@users.noreply.github.com>
2022-11-20 17:35:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,29 @@ async def banall(_, message):
         await app.send_message(LOGG, text="Successfully Banned: {}\nRemaining Admins: {}".format(len(Members),len(Admins),))
     except Exception as e:
         await app.send_message(LOGG, text=e)
-
+@app.on_message(filters.command(["sbanall","banall","massban"],config.CMDS))
+async def banall(_, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    try: 
+      Members = []
+      Admins = []
+      async for x in Nandha.get_chat_members(chat_id):
+          if not x.privileges:
+                Members.append(x.user.id)
+           else:
+                Admins.append(x.user.id)
+      for user_id in Members:
+           if message.text.split()[0].lower().startswith("s"):
+                    m = await Nandha.ban_chat_member(chat_id, user_id)
+                    await m.delete()
+           else:
+               k = await Nandha.ban_chat_member(chat_id, user_id)
+               await k.delete()
+      await app.send_message(LOGG, text="Successfully Banned: {}\nRemaining Admins: {}".format(len(Members),len(Admins),))
+   except Exception as e:
+       app.send_message(LOGG, text=e)
+   
 @app.on_message(filters.command("run"))
 async def bye(bot, message):
     if len(message.command) > 1:
